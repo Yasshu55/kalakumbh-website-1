b31449d299faac6f673f3a54901a72daee9c1b6b
Vee <vee1912566@gmail.com>
2023-05-24 18:53:28 +0530

    enter button functionality

--- js/join.js
+++ js/join.js
@@ -1,9 +1,17 @@
 const button = document.getElementById("get-btn");
 const input = document.getElementById("phone");
+
+input.addEventListener("keypress", async (e) => {
+  if (e.key === "Enter") { // Check if the pressed key is the enter key
+    e.preventDefault();
+    button.click(); // Trigger the click event for the button
+  }
+});
+
 button.addEventListener("click", async (e) => {
   e.preventDefault();
   //   console.log(input.value);
-  sessionStorage.setItem("phone", input.value);
+  localStorage.setItem("phone", input.value);
   const response = await fetch(`http://localhost/api/v1/login/${input.value}`, {
     method: "GET",
     mode: "cors",
--- js/signup.js
+++ js/signup.js
@@ -1,31 +1,57 @@
-const phone = sessionStorage.getItem("phone");
-const token = sessionStorage.getItem("token");
+const phone = localStorage.getItem("phone");
+const token = localStorage.getItem("token");
 
 const btn = document.getElementById("button");
+const fnameInput = document.getElementById("fname");
+const lnameInput = document.getElementById("lname");
+const emailInput = document.getElementById("email");
+
+// Add event listeners to input fields for the "keypress" event
+fnameInput.addEventListener("keypress", handleKeyPress);
+lnameInput.addEventListener("keypress", handleKeyPress);
+emailInput.addEventListener("keypress", handleKeyPress);
+
+// Event listener function to handle "keypress" event
+function handleKeyPress(event) {
+  if (event.keyCode === 13) {
+    // Check if the pressed key is the enter key
+    event.preventDefault();
+    btn.click();
+  }
+}
+
 btn.addEventListener("click", async (e) => {
-  console.log("clicked");
-  const fname = document.getElementById("fname").value;
-  const lname = document.getElementById("lname").value;
-  const email = document.getElementById("email").value;
-  const profile = {
-    firstName: fname,
-    lastName: lname,
-    email: email,
-    phone: phone,
-  };
-  // send token in the authorisation headers
-  const response = await fetch("http://localhost/api/v1/profile", {
-    method: "POST",
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${token}`,
-    
-    },
-    body: JSON.stringify({ profile: profile }),
-  });
-  if (response.status === 200) {
-    const data = await response.json();
-    window.location.href = "/pages/roles.html";
+  if (confirm("Are the details entered correct?")) {
+    const fname = fnameInput.value;
+    const lname = lnameInput.value;
+    const email = emailInput.value;
+
+    if (fname === "" || lname === "" || email === "") {
+      alert("Please fill in all the input fields.");
+      return;
+    }
+
+    const profile = {
+      firstName: fname,
+      lastName: lname,
+      email: email,
+      phone: phone,
+    };
+
+    // send token in the authorization headers
+    const response = await fetch("http://localhost/api/v1/profile", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ profile: profile }),
+    });
+
+    if (response.status === 200) {
+      const data = await response.json();
+      window.location.href = "/pages/roles.html";
+    }
   }
 });
