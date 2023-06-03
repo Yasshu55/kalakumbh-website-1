6c484cbf4830fc93b9c2519fbeb415adffdb271f
Sri-1227 <srinivasmekala1227@gmail.com>
2023-06-03 12:44:04 +0530

    server link changed in join.js

--- js/join.js
+++ js/join.js
@@ -3,26 +3,28 @@ const input = document.getElementById("phone");
 const token = localStorage.getItem("token");
 const phoneNo = localStorage.getItem("phone");
 
-
 input.addEventListener("keypress", async (e) => {
-  if (e.key === "Enter") { // Check if the pressed key is the enter key
+  if (e.key === "Enter") {
+    // Check if the pressed key is the enter key
     e.preventDefault();
     button.click(); // Trigger the click event for the button
   }
 });
 
-
 button.addEventListener("click", async (e) => {
   e.preventDefault();
   //   console.log(input.value);
   localStorage.setItem("phone", input.value);
-  const response = await fetch(`http://localhost/api/v1/login/${input.value}`, {
-    method: "GET",
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  const response = await fetch(
+    `https://kalakumbh-server.kalakumbh.org/api/v1/login/${input.value}`,
+    {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
   // if status is 200 then redirect to otp page
   if (response.status === 200) {
     window.location.href = `/pages/otp.html`;
